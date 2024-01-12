107f9b613c1b7fcce3af68d8e390911f7fd379aa
pranav04125 <pranavarun05@gmail.com>
2024-01-13 03:18:14 +0400

    Finished Sign Up and Login functionalities
    Signing up works with checks for already in use username and email and redirects to login on success

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def login():
     else:
         incorrect_creds = False
 
+    if "new_user_added" in session:
+        new_user_added = True # For display of user added please login message after signup
+    else:
+        new_user_added = False
+
     # Redirects to profile page if already logged in
     if "user_id" in session:
         return redirect(url_for("profile"))
@@ -49,8 +54,54 @@ def login():
             session["incorrect_creds"] = True  # Added to display message on refresh
             return redirect("login")
 
-    return render_template("login.html", incorrect_creds=incorrect_creds)
+    return render_template("login.html", incorrect_creds=incorrect_creds, new_user_added=new_user_added)
 
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    """
+    SignUp page that checks if username and email already exists and adds to database if it doesn't before redirecting to login Page
+    """
+    print(session.items(), request.method)
+    if "username_alr_exists" in session:
+        username_exists = True  # For display of already used credentials message on signup page (Only if signup retry)
+        session.pop("username_alr_exists", None)
+    else:
+        username_exists = False
+
+    if "email_alr_exists" in session:
+        email_exists = True
+        session.pop("email_alr_exists", None)
+    else:
+        email_exists = False
+
+    # Redirects to profile page if already logged in
+    if "user_id" in session:
+        return redirect(url_for("profile"))
+
+    if request.method == "POST":
+        username = request.form.get("new_username", None)
+        password = request.form.get("new_password", None)
+        email = request.form.get("email", None)
+        print(username, password, email)
+        # Checks for already used email and username before registering (Email has preference on message if both)
+        cursor.execute("Select * FROM users WHERE email=%s", (email,))
+        if cursor.fetchone():
+            print("asdf")
+            session["email_alr_exists"] = True
+            return redirect("signup")
+
+        cursor.execute("Select * FROM users WHERE username=%s", (username,))
+        if cursor.fetchone():
+            print("asdf")
+            session["username_alr_exists"] = True
+            return redirect("signup")
+
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+        session["new_user_added"] = True
+        return redirect("login")
+
+    return render_template("signup.html", username_exists=username_exists, email_exists=email_exists)
 
 @app.route("/profile")
 def profile():
