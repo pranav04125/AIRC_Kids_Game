a2307632794b1fad4466d6f47baccd00a17ad427
pranav04125 <pranavarun05@gmail.com>
2024-01-13 00:43:55 +0400

    created db config and linked database with project

---
+++ dbconfig.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+# Change to your database credentials
+db_conf = {
+    "host" : "localhost",
+    "user" : "root",
+    "password" : "pranav05",
+    "database" : "AIRC_Kids_Game"
+}
+
+connection = mysql.connector.connect(**db_conf)
+connection.autocommit = True
+
+# Cursor will always return dictionary type as its easier to manage than indexes
+# Buffered prevents unread results errors by always auto fetching all rows after select statements
+cursor = connection.cursor(dictionary=True, buffered=True)
